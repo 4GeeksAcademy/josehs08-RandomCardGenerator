060c419c04862031202c1ca484b28e937067e4b3
josehs08 <168867459+josehs08@users.noreply.github.com>
2024-06-08 04:00:37 +0000

    Adelanto

--- src/app.js
+++ src/app.js
@@ -1,11 +1,16 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+const carta = {
+  numero: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"],
+  pinta: ["Diamantes", "Corazon", "Trebol", "Picas"]
 };
+
+function generarCarta() {
+  let cartas = "";
+  cartas +=
+    generarNumeroRandom(carta.numero) + " " + generarNumeroRandom(carta.pinta);
+  return cartas;
+}
+function generarNumeroRandom(str) {
+  return str[Math.floor(Math.random() * str.length)];
+}
+
+let cartaGenerada = generarCarta();
