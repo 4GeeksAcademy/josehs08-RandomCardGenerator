012503c4d1e84639efbef179f1c1b582f875e34f
josehs08 <168867459+josehs08@users.noreply.github.com>
2024-06-11 17:10:53 +0000

    adelanto

--- src/app.js
+++ src/app.js
@@ -1,6 +1,11 @@
 let carta = {
   numero: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"],
-  pinta: ["Diamantes", "Corazon", "Trebol", "Picas"]
+  pinta: [
+    "bi bi-suit-diamond-fill text-danger",
+    "bi bi-suit-heart-fill text-danger",
+    "bi bi-suit-club-fill",
+    "bi bi-suit-spade-fill"
+  ]
 };
 
 function generarCarta() {
@@ -9,31 +14,35 @@ function generarCarta() {
   cartas.pinta = generarNumeroRandom(carta.pinta);
   return cartas;
 }
+
 function generarNumeroRandom(str) {
   return str[Math.floor(Math.random() * str.length)];
 }
 
+let root = document.querySelector("#root");
+root.className = "container-fluid d-flex ";
+
 function addCartaDOM() {
   let cartaRandom = generarCarta();
 
-  let contenedor = document.createElement("div");
-  contenedor.className = "d-flex flex-column bg-white p-3 w-25";
+  let cardContainer = document.createElement("div");
+  cardContainer.className = "d-flex flex-column bg-white p-3 rounded";
+  cardContainer.style.width = "30%";
 
   let numeroCarta = document.createElement("p");
   numeroCarta.className = "text-center display-1";
   numeroCarta.textContent = cartaRandom.numero;
 
   let pintaCartaArriba = document.createElement("i");
-  pintaCartaArriba.className =
-    "fa-solid fa-heart fa-2xl text-danger text-start";
-
   let pintaCartaAbajo = document.createElement("i");
-  pintaCartaAbajo.className = "fa-solid fa-heart fa-2xl text-danger text-end";
+  pintaCartaArriba.className = `${cartaRandom.pinta} text-start`;
+  pintaCartaAbajo.className = `${cartaRandom.pinta} text-end`;
 
-  contenedor.appendChild(pintaCartaArriba);
-  contenedor.appendChild(numeroCarta);
-  contenedor.appendChild(pintaCartaAbajo);
+  cardContainer.appendChild(pintaCartaArriba);
+  cardContainer.appendChild(numeroCarta);
+  cardContainer.appendChild(pintaCartaAbajo);
 
-  const currentDiv = document.querySelector("#div1");
-  document.body.insertBefore(contenedor, currentDiv);
+  root.appendChild(cardContainer);
 }
+
+addCartaDOM();
