d90d59b78ce19297a731fa5e7cf7bad76a9e38be
josehs08 <168867459+josehs08@users.noreply.github.com>
2024-06-11 22:23:01 +0000

    Avances en cambio w y h

--- src/app.js
+++ src/app.js
@@ -1,14 +1,14 @@
-let carta = {
-  numero: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"],
-  pinta: [
-    "bi bi-suit-diamond-fill text-danger",
-    "bi bi-suit-heart-fill text-danger",
-    "bi bi-suit-club-fill",
-    "bi bi-suit-spade-fill"
-  ]
-};
-
+//Logica para crear carta random
 function generarCarta() {
+  let carta = {
+    numero: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"],
+    pinta: [
+      "bi bi-suit-diamond-fill text-danger",
+      "bi bi-suit-heart-fill text-danger",
+      "bi bi-suit-club-fill",
+      "bi bi-suit-spade-fill"
+    ]
+  };
   let cartas = {};
   cartas.numero = generarNumeroRandom(carta.numero);
   cartas.pinta = generarNumeroRandom(carta.pinta);
@@ -18,16 +18,52 @@ function generarCarta() {
 function generarNumeroRandom(str) {
   return str[Math.floor(Math.random() * str.length)];
 }
+//
 
+//Logica DOM
 let root = document.querySelector("#root");
-root.className = "container-fluid d-flex ";
+root.className = "container-fluid d-flex row gap-3";
+
+//Crear div de "opciones"
+let optionsDiv = document.createElement("div");
+optionsDiv.className = "col-12 d-flex gap-3 justify-content-center";
+root.appendChild(optionsDiv);
+
+//Boton para crear carta nueva.
+let btnNewCard = document.createElement("button");
+btnNewCard.className = "btn btn-primary";
+btnNewCard.textContent = "Crear nueva carta";
+btnNewCard.addEventListener("click", addCartaDOM);
+
+optionsDiv.appendChild(btnNewCard);
+//
+
+//Crear carta nueva cada 10 segundos
+//
+
+//Input cambio width y height
+let inputWidth = document.createElement("input");
+let inputHeight = document.createElement("input");
+optionsDiv.appendChild(inputWidth);
+optionsDiv.appendChild(inputHeight);
+inputWidth.value = "30%";
+inputHeight.value = "100%";
+inputWidth.addEventListener("input", changeAllSizes);
+function changeAllSizes() {
+  for (let index = 1; index < root.children.length; index++) {
+    root.children[index].style.width = inputWidth.value;
+    root.children[index].style.height = inputHeight.value;
+  }
+}
+//
 
 function addCartaDOM() {
   let cartaRandom = generarCarta();
 
   let cardContainer = document.createElement("div");
   cardContainer.className = "d-flex flex-column bg-white p-3 rounded";
-  cardContainer.style.width = "30%";
+  cardContainer.style.width = inputWidth.value;
+  cardContainer.style.height = inputHeight.value;
 
   let numeroCarta = document.createElement("p");
   numeroCarta.className = "text-center display-1";
@@ -44,5 +80,3 @@ function addCartaDOM() {
 
   root.appendChild(cardContainer);
 }
-
-addCartaDOM();
